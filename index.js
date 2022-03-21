42b772c2d623305d4cc15d44191331485252fd20
salman2090 <salman20909@gmail.com>
2022-03-21 22:26:48 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+require('dotenv').config();
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectId;
+
+const port = 5000;
+
+// mongodb connect
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ncbah.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.use(cors());
+app.use(express.json())
+
+async function run() {
+    try{
+        await client.connect();
+        const database = client.db('mediversal_hospital');
+        const servicesCollection = database.collection("services");
+        const doctorsCollection = database.collection("doctors");
+        const departmentsCollection = database.collection('departments');
+
+
+        // get services
+          app.get("/services", async (req, res) => {
+            const result = await servicesCollection.find({}).toArray();
+            res.send(result);
+        });
+
+        // get doctors
+          app.get("/doctors", async (req, res) => {
+            const result = await doctorsCollection.find({}).toArray();
+            res.send(result);
+        });
+        
+        // get departments
+          app.get("/departments", async (req, res) => {
+            const result = await departmentsCollection.find({}).toArray();
+            res.send(result);
+        });
+
+        
+    }
+    finally {
+        // await client.close();
+    }
+}
+ 
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('Hello Winged Wheels!')
+  })
+   
+  app.listen(port, () => {
+    console.log(`listening at ${port}`)
+  })
